55d7fb04c65637a372f742666d4d46aa5ed844aa
Ayomide Olusanjo Olomi <ayomideolomi1@gmail.com>
2023-04-17 13:22:43 -0700

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  * main - It will print the file name of the program that is getting compiled
+  *
+  * Return: Always zero
+  */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
